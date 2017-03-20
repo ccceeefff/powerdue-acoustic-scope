75c5323c93e6a3ee4353c8e991cd1dec4337b60f
Cef Ramirez <cef.ramirez@gmail.com>
2017-03-20 00:54:15 -0700

    adding tcp streams

--- renderer.js
+++ renderer.js
@@ -5,6 +5,9 @@
 var fs = require("fs");
 var SerialPort = require('serialport');
 var Plotly = require('plotly.js/lib/core');
+var http = require('http');
+var net = require('net');
+const {dialog} = require('electron').remote;
 
 Number.prototype.clamp = function(min, max) {
   return Math.min(Math.max(this, min), max);
@@ -15,13 +18,24 @@ var serialPortSelect = document.getElementById('serialportSelect');
 var plotDiv = document.getElementById('plot');
 
 var time = [];
-var data = [];
+var serialData = [];
+var traces = 0;
+
+var buffers = {};
+var traceNames = [];
+traceNames.push("Serial");
 
 function initPlot(){
-  Plotly.plot(plotDiv, [{
-    y: []
+  var initTraces = [];
+  for(var i=0; i < traces+1; i++){
+    initTraces.push({
+      y: [],
+      name: traceNames[i]
+    });
   }
-  ], {
+  
+  Plotly.plot(plotDiv, initTraces,
+  {
     xaxis: {title: 'Time (us)'},
     yaxis: {title: 'Magnitude'},
     margin: {t: 0},
@@ -30,10 +44,10 @@ function initPlot(){
 }
 initPlot();
 
-function render(y){
+function render(y, index){
   Plotly.restyle(plotDiv, {
     y: [y]
-  }, [0]);
+  }, [index]);
   // Plotly.extendTraces(plotDiv, {y: [y]}, [0]);
 }
 
@@ -42,12 +56,27 @@ exports.clearPlots = function(){
   initPlot();
 }
 
-function addData(buffer){
-  data = data.concat(Array.from(buffer));
-  if(data.length > 2048){
-    data = data.slice(buffer.length);
+exports.exportData = function(){
+  dialog.showSaveDialog({title: "powerdue-out.csv"}, function(filename){
+    if(filename != null){
+      console.log("writing file to: " + filename);
+    }
+  });
+  // write timestamped data buffers into csv file 
+}
+
+function addData(buffer, index){
+  var dataBuf = buffers["" + index];
+  if(dataBuf == null){
+    dataBuf = [];
+    buffers["" + index] = dataBuf;
   }
-  render(data);
+  dataBuf = dataBuf.concat(Array.from(buffer));
+  if(dataBuf.length > 2048){
+    dataBuf = dataBuf.slice(buffer.length);
+  }
+  buffers["" + index] = dataBuf;
+  render(dataBuf, index);
 }
 
 exports.onSerialRefresh = function(){
@@ -72,7 +101,7 @@ exports.onSerialOpen = function(){
   
   port = new SerialPort(chosenPort, { 
     autoOpen: false,
-    baudRate: 115200,
+    baudRate: 1050000,
     parser: SerialPort.parsers.byteLength(256)
   });
   port.open(function (err) {
@@ -88,7 +117,7 @@ exports.onSerialOpen = function(){
 
   port.on('data', function(data){
     var buf = new Uint16Array(data.buffer, data.byteOffset, data.byteLength/Uint16Array.BYTES_PER_ELEMENT);
-    addData(buf);
+    addData(buf, 0);
   });
 
   port.on('close', function(){
@@ -108,3 +137,28 @@ exports.onSerialClose = function(){
   port.close();
   port = null;
 }
+
+// const server = http.createServer((req, res) =>{
+//   console.log(req);
+//   if(req.method == "GET"){
+//     console.log(req.connection.remoteAddress);
+//   }
+//   res.end();
+// });
+const server = net.createServer((c) => {
+  traces++;
+  c.traceNumber = traces;
+  Plotly.addTraces(plotDiv, {y: [], name: c.remoteAddress});
+  traceNames.push(c.remoteAddress);
+  c.on('end', () => {
+    console.log("client disconnected");
+  });
+  c.on('data', (data) => {
+    var buf = new Uint16Array(data.buffer, data.byteOffset, data.byteLength/Uint16Array.BYTES_PER_ELEMENT);
+    addData(buf, c.traceNumber);
+  });
+});
+server.on('error', (err) => {
+  console.log("error: " + err);
+});
+server.listen(4000);
